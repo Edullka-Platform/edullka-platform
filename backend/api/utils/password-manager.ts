f5cced0f7ddcb7280a3af8b19564373e63441c0f
joseniquen08 <j.armando0807@gmail.com>
2022-07-26 21:39:10 -0500

    update: async functions and request type (createUserController)

--- backend/api/utils/password-manager.ts
+++ backend/api/utils/password-manager.ts
@@ -1,10 +1,10 @@
 import bcrypt from 'bcrypt';
 
 export const passwordManager = {
-  hashPassword: async (password: string) => {
+  hashPassword: async (password: string): Promise<string> => {
     return await bcrypt.hash(password, 10);
   },
-  comparePassword: (password: string, hash: string) => {
-    return bcrypt.compare(password, hash);
+  comparePassword: async (password: string, hash: string): Promise<boolean> => {
+    return await bcrypt.compare(password, hash);
   },
 };
